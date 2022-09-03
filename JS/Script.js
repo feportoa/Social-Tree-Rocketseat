5c9eb4bf409ce14a177c6079f18497bb61c076ae
Fernando Porto <feporto.a@gmail.com>
2022-09-02 23:43:06 -0300

    added functional Light/Dark modes

--- JS/Script.js
+++ JS/Script.js
@@ -1,7 +1,30 @@
+    var darkModeSwitch = document.querySelector('.moon');
+    var body = document.querySelector('body');
+    var lightModeSwitch = document.querySelector('.sun');
+    var linkItem = document.querySelectorAll('.flexLink');
+    var headerCircle = document.querySelector('.banner');
+    var instagramLetters = document.querySelector('header p');
+
+
 function switchModeDark(){
 
+    body.classList.toggle('dark');
+    darkModeSwitch.classList.toggle('disabled');
+    lightModeSwitch.classList.toggle('disabled');
+    for(c=0;c<linkItem.length;c++){
+        linkItem[c].classList.toggle('darkPurple');
+    }
+    headerCircle.id = 'headerImage';
+    instagramLetters.classList.toggle('darkPurple');
 }
 
 function switchModeBright(){
-    
+    body.classList.toggle('dark');
+    darkModeSwitch.classList.toggle('disabled');
+    lightModeSwitch.classList.toggle('disabled');
+    for(c=0;c<linkItem.length;c++){
+        linkItem[c].classList.toggle('darkPurple');
+    }
+    headerCircle.removeAttribute('id'); 
+    instagramLetters.classList.toggle('darkPurple');
 }
