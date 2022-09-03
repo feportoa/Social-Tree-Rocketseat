34445b8aa8528f6b6855afed8b40dbb96e8b4f0e
Fernando Porto <feporto.a@gmail.com>
2022-09-02 22:42:16 -0300

    test - modified js

--- JS/Script.js
+++ JS/Script.js
@@ -1,39 +1,7 @@
-// function switchMode(){
-//     let body = document.getElementsByName('body');
-//     let links = document.getElementsByClassName('flexLink');
-//     screenSwitch();
-// }
+function switchModeDark(){
 
-// function screenSwitch(){
-//     for(let c=0; c<=1;c++){
-
-//     }
-// }
-
-function switchMode() {
-    var c;
-    if(c=1){
-        switchBlack(c);
-    } else {
-        switchWhite(c);
-    }
 }
 
-function switchBlack(c) {
-    if(c=1){
-        c = 0;
-        /* add classlists */
-    } else {
-        return 1;
-    }
-}
-
-function switchWhite(c) {
-    var c = 0;
-    if(c=0){
-        c = 1;
-        /* add classlists */
-    } else {
-        return 1;
-    }
+function switchModeBright(){
+    
 }
